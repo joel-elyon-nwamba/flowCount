c64e8bdc92fcbb6654599587dccfb5b456317627
joel-nwamba <joel.elyon.nwamba@gmail.com>
2025-04-30 19:12:43 -0400

    Add count to increase and decrease

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
 const btnMinus = document.getElementById("btn-minus");
 const btnPlus = document.getElementById("btn-plus");
 const num = document.getElementById("num");
+let count = 0;
+
+function increaseCount() {
+    count++
+    num.textContent =  count;
+}
+
+increaseCount();
+
+function decreaseCount() {
+    count--;
+    num.textContent = count;
+}
+
+decreaseCount();
+
+btnPlus.addEventListener("click", increaseCount);
+
+btnMinus.addEventListener("click", decreaseCount);
 
