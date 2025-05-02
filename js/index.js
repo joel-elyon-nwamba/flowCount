8b37ef7bbac4f62797f1c1670ecb085ce5207f4e
joel-nwamba <joel.elyon.nwamba@gmail.com>
2025-05-01 22:05:42 -0400

    Saving amount of number updated

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 const btnMinus = document.getElementById("btn-minus");
 const btnPlus = document.getElementById("btn-plus");
 const num = document.getElementById("num");
+const saveBtn = document.getElementById("save-btn");
+const paragraph = document.createElement("p");
 let count = 0;
+const flowContainer = document.querySelector(".flow-container");
+paragraph.classList.add("saved-count");
+flowContainer.appendChild(paragraph);
 
 function increaseCount() {
     count++
@@ -11,11 +16,25 @@ function increaseCount() {
 increaseCount();
 
 function decreaseCount() {
+    if(count >= 1)
     count--;
     num.textContent = count;
 }
 
 decreaseCount();
+
+// Create save button
+
+function saveCount() {
+    let countSum = count;
+    paragraph.textContent = `Saved count: ${countSum}`;
+    num.textContent = 0;
+    count = 0;
+}
+
+saveCount();
+
+saveBtn.addEventListener("click", saveCount);
 
 btnPlus.addEventListener("click", increaseCount);
 
